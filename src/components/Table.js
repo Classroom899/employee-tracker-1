79793b10cb4be61411c2fc19433ef743e6c79d29
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-13 18:07:41 -0700

    table made

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,18 +4,19 @@ import React from "react";
 
 
 export default function ({ columns, data }) {
+    const TableRow = ({rowData, tag}) => { // created mini component and tag (HTML tag) is used to render the data
+        return rowData.map (cell => tag === "th" ? <th>{cell}</th>: <td>{cell}</td>)
+
+    } // declared an inner component to be used by the main component 
     return (<>
         <table>
             <thead>
                 <tr>
-
+                    <TableRow rowData={columns} tag="th" />
                 </tr>
-
-
-
             </thead>
             <tbody>
-
+                    {data.map(row => <TableRow rowData={row} tag="td" />)}
             </tbody>
         </table>
     </>); // empty div
