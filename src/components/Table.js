592c1535ddd37d4b98bfb68b29997593ff451c20
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-13 17:48:20 -0700

    created tableRow first data to be passed through into it

---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+// has to be capitalized 
+import React from "react";
+
+
+
+export default function ({ columns, data }) {
+    return (<>
+        <table>
+            <thead>
+                <tr>
+
+                </tr>
+
+
+
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </>); // empty div
+}
---
+++ src/containers/notes.js
@@ -0,0 +1,3 @@
+// takes the component into the container making a new container then exporting it to an index and then using that for the overall UI
+// want to use our componenets over and over again
+// if the data is wrapped inside the container, then we can't use that
