dcc971d7c06072b39b409311f1e6c6d94a0aa2ff
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-08-13 22:18:32 -0700

    added useState for employees for sorting data by alphabetical order and map

--- src/containers/EmployeeDirectory.js
+++ src/containers/EmployeeDirectory.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // external library 
 import Table from "../components/Table"
@@ -7,8 +7,25 @@ import Table from "../components/Table"
 
 // internal library 
 export default function ({ headers, employees }) {
-    return(<>
-    <Table columns={headers} data={employees}/>
+    const [state, setState] = useState(employees)
+    const sortByName = () => {
+        const data = state.map(employee => employee)
+        console.log(data);
+        data.sort((e, em) => {
+            if (e[0] < em[0]) {
+                return -1;
+            } else if (e[0] > em[0]) { // sorting by alphabetical here
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+
+        setState(data);
+    }
+    return (<>
+        <button onClick={sortByName}>Sort By Name</button>
+        <Table columns={headers} data={state} />
 
     </>)
 }
